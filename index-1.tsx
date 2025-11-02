d176a085d2abbc680e768b498bb49ac7dcf2abbf
sigortapolice <firatxwork@gmail.com>
2025-11-02 19:17:16 +0300

    Delete index-1.tsx

--- index-1.tsx
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank.
-// The application has been migrated to a self-contained vanilla JavaScript implementation in index.html,
-// and the React bootstrapping logic is no longer required.
