cdfce7690d9709ff31ce887016570225b3f191e8
sigortapolice <firatxwork@gmail.com>
2025-11-06 12:58:46 +0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,3 @@
+// The application logic has been moved into a <script> tag directly in index.html.
+// This was done to resolve file loading issues in the deployment environment.
+// This file is no longer used and can be safely removed.
